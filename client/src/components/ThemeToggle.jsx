c6bac6e4fb0b542c1c254500bb122faa741cd484
artdesignmethod <24868045+artdesignmethod@users.noreply.github.com>
2024-09-06 12:08:08 -0400

    Added animation for theme toggle button.

--- client/src/components/ThemeToggle.jsx
+++ client/src/components/ThemeToggle.jsx
@@ -6,12 +6,20 @@ const ThemeToggle = () => {
   const { darkTheme, toggleDarkTheme } = useDashboardContext();
 
   return (
-    <div className="theme-toggle-container" onClick={toggleDarkTheme}>
-      <img
-        src={!darkTheme ? lightThemeIcon : darkThemeIcon}
-        className="theme-icon"
-        alt="afternoon and evening icons"
-      />
+    <div className="theme-toggle" onClick={toggleDarkTheme}>
+      {!darkTheme ? (
+        <img
+          src={lightThemeIcon}
+          className="light-theme-icon"
+          alt="sun icon, light theme mode"
+        />
+      ) : (
+        <img
+          src={darkThemeIcon}
+          className="dark-theme-icon"
+          alt="star and moon icon, dark theme mode"
+        />
+      )}
     </div>
   );
 };
