1b5070e1efcce17fed1f50365cdc394faa0b0766
artdesignmethod <24868045+artdesignmethod@users.noreply.github.com>
2024-09-09 13:29:49 -0400

    Implemented logout functionality in authController. Logout route successfully tested.

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-import { register, login } from "../controllers/authController.js";
+import { register, login, logout } from "../controllers/authController.js";
 
 import {
   validateLoginInput,
@@ -11,5 +11,7 @@ import {
 router.post("/register", validateRegisterInput, register);
 
 router.post("/login", validateLoginInput, login);
+
+router.get("/logout", logout);
 
 export default router;
